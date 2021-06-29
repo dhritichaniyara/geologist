b06557418f5e1db988e6c8b9e28badd2187196ad
dhritichaniyara <76730093+dhritichaniyara@users.noreply.github.com>
2021-06-29 16:11:34 +0530

    Add files via upload

---
+++ Iron.js
@@ -0,0 +1,34 @@
+class Iron{
+    constructor(x,y,width,height){ 
+    
+	// assign options to the rubber ball
+	var options={
+		restitution:0.8,
+		friction:3,
+		density:30
+	}
+
+		this.x=x;
+		this.y=y;
+		this.width=width;
+        this.height=height;
+		this.body=Bodies.rectangle(this.x, this.y, width,height, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var pos=this.body.position;
+            var angle=this.body.angle;		
+			push();
+			translate(pos.x, pos.y);
+            rotate(angle);
+			rectMode(CENTER);
+			strokeWeight(4);
+			stroke("brown");
+			fill("brown");
+			//draw the ellipse here to display the rubber ball
+			rect(0,0,this.width,this.height);
+			pop();
+	}
+}
